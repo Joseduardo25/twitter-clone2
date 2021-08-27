896ce7fb80c462f2d1b9bc168cdc3915ff98cec3
eduardo <jocdwardo25@gmail.com>
2021-08-27 14:00:38 -0500

    maquetado terminado

---
+++ src/Post.js
@@ -0,0 +1,44 @@
+import { Avatar } from '@material-ui/core'
+import VerifiedUserIcon from '@material-ui/icons/VerifiedUser';
+import React from 'react'
+import './Post.css'
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline'
+import RepeatIcon from '@material-ui/icons/Repeat'
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
+import PublishIcon from '@material-ui/icons/Publish'
+
+function Post({ displayName, username, verified, text, image, avatar }) {
+  return (
+    <div className='post'>
+      <div className='post__avatar'>
+        <Avatar src='https://pbs.twimg.com/profile_images/1420572812193026050/W8boMMDn_normal.jpg' />
+      </div>
+      <div className='post__body'>
+        <div className='post__header'>
+          <div className='post__headerText'>
+            <h3>
+              Rafeh Qazi {''}
+              <span className='post__headerSpecial'>
+                <VerifiedUserIcon className='post__badge' /> @cleverqazi
+              </span>
+            </h3>
+          </div>
+          <div className='post__headerDescription'>
+            <p>I challenge you to build a Twitter Clone with React</p>
+          </div>
+        </div>
+        <img 
+          src='https://pbs.twimg.com/media/E9y7zkjXIAYX1KO?format=jpg&name=small'
+        />
+        <div className='post__footer'>
+          <ChatBubbleOutlineIcon fontSize='small'/>
+          <RepeatIcon fontSize='small'/>
+          <FavoriteBorderIcon fontSize='small'/>
+          <PublishIcon fontSize='small'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
